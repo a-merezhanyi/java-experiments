49bd9fe695489e430364352b7379be20cdfadaa5
A. Merezhanyi <a.merezhanyi@gmail.com>
2022-12-03 20:44:14 +0100

    started project

---
+++ learn-sprint-boot-3/src/main/java/com/in28minutes/springboot/learnsprintboot/LearnSprintBootApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.learnsprintboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSprintBootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LearnSprintBootApplication.class, args);
+  }
+
+}
---
+++ learn-sprint-boot-3/src/test/java/com/in28minutes/springboot/learnsprintboot/LearnSprintBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.learnsprintboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSprintBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
